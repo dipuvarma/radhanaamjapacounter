fa5e9b8381c33c8f4da5c3a0657ef16427bd8c35
RaviCoderWallah <raviverma05042023@gmail.com>
2026-06-06 08:36:34 +0530

    ui: volume mute and unmute toggle bar and select mantra dropdown

--- src/features/chanting-engine/components/CounterPanel.jsx
+++ src/features/chanting-engine/components/CounterPanel.jsx
@@ -7,7 +7,7 @@ const CounterPanel = () => {
   const { japaCount, malaCount, elapseSeconds } = useJapaState();
 
   return (
-    <div className="my-2 flex justify-center">
+    <div className="mt-2 flex justify-center">
       <div className="flex items-center justify-center gap-4 bg-gradient-to-r from-amber-100 via-gray-50 to-amber-100 outline-1 outline-amber-300 py-1 px-4 shadow-sm rounded-full">
         <p className="text-sm text-amber-900">Japa: {japaCount}</p>
         <p className="text-sm text-amber-900">Mala: {malaCount}</p>
--- src/features/chanting-engine/components/HeroSection.jsx
+++ src/features/chanting-engine/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { BiSolidVolumeFull, BiSolidVolumeMute } from "react-icons/bi";
 
 import { useJapaState } from "@/hooks/useJapaState";
 import { CounterPanel, DeityDisplay } from "..";
@@ -12,10 +13,56 @@ const HeroSection = () => {
 
   return (
     <section onClick={handleJapaCount} className="w-full select-none">
-      <section className=" max-w-4xl mx-auto px-4 py-8">
-        <h1 className="primary-heading text-center font-semibold text-amber-900 cinzel-heading">
-          Radhe Radhe
-        </h1>
+      <section className="  mx-auto px-4 py-4">
+        <div className="flex items-center justify-between">
+          {/* Audio Mute and Unmute Toggle  */}
+          <div className="flex items-center gap-2">
+            <BiSolidVolumeMute className="text-xl text-amber-950" />
+            <div className="w-14 h-6 bg-gray-50 rounded-full flex items-center p-1 cursor-pointer">
+              <div className="w-5 aspect-square rounded-full bg-amber-900"></div>
+            </div>
+            <BiSolidVolumeFull className="text-xl text-amber-950" />
+          </div>
+
+          {/* Current Mantra Title  */}
+          <h1 className="primary-heading text-center font-semibold text-amber-900 cinzel-heading">
+            Radhe Radhe
+          </h1>
+
+          {/* Select Mantra Dropdown  */}
+          <div className="relative font-poppins">
+            <select
+              name=""
+              id=""
+              className="appearance-none bg-gray-50 rounded-full pl-4 pr-8 py-1.5 text-sm font-medium text-amber-900 shadow-sm focus:outline-none focus:ring-1 focus:ring-saffron/50 transition-all duration-300 cursor-pointer"
+            >
+              <option value="" className="bg-amber-50 text-amber-900 text-sm">
+                Radhe Radhe
+              </option>
+              <option value="" className="bg-amber-50 text-amber-900 text-sm">
+                Om Nam Shivaye
+              </option>
+              <option value="" className="bg-amber-50 text-amber-900 text-sm">
+                Ram Ram
+              </option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+              <svg
+                className="w-4 h-4 text-saffron"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2.5"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
 
         <CounterPanel />
         <DeityDisplay />
