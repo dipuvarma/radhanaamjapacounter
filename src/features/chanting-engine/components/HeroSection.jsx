40f5fa00b642b028c27c9bfc53a4032e949f6ee8
RaviCoderWallah <raviverma05042023@gmail.com>
2026-06-24 09:26:25 +0530

    feat: add radha rahda sound on chating

--- src/features/chanting-engine/components/HeroSection.jsx
+++ src/features/chanting-engine/components/HeroSection.jsx
@@ -5,12 +5,28 @@ import { BiSolidVolumeFull, BiSolidVolumeMute } from "react-icons/bi";
 import { useJapaState } from "@/hooks/useJapaState";
 import { CounterPanel, DeityDisplay } from "..";
 import Link from "next/link";
+import useSound from "use-sound";
+import { useState } from "react";
 
 const HeroSection = () => {
   const { handleIncreaseJapaCount } = useJapaState();
+  const [isSoundOn, setIsSoundOn] = useState(false);
 
-  const handleJapaCount = () => {
+  const [play] = useSound("/audio/radha-radha.mp3", {
+    volume: 0.5,
+    interrupt: true,
+  });
+
+  const handleSoundStatus = (e) => {
+    e.stopPropagation();
+    setIsSoundOn((prev) => !prev);
+  };
+
+  const handleJapaCount = (e) => {
     handleIncreaseJapaCount();
+    if (isSoundOn) {
+      play();
+    }
   };
 
   return (
@@ -18,10 +34,17 @@ const HeroSection = () => {
       <section className="mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           {/* Audio Mute and Unmute Toggle  */}
-          <div className="flex items-center gap-2">
+          <div
+            className="flex items-center gap-2"
+            onClick={(e) => handleSoundStatus(e)}
+          >
             <BiSolidVolumeMute className="text-xl text-amber-950" />
-            <div className="w-14 h-6 bg-gray-50 rounded-full flex items-center p-1 cursor-pointer">
-              <div className="w-5 aspect-square rounded-full bg-amber-900"></div>
+            <div
+              className={`${isSoundOn ? "bg-amber-900 justify-end" : "bg-gray-100"} w-14 h-6  transition-all duration-300 rounded-full flex items-center p-1 cursor-pointer`}
+            >
+              <div
+                className={`${isSoundOn ? "bg-gray-50" : "bg-gray-600"} w-5 aspect-square rounded-full `}
+              ></div>
             </div>
             <BiSolidVolumeFull className="text-xl text-amber-950" />
           </div>
@@ -36,6 +59,7 @@ const HeroSection = () => {
             <select
               name=""
               id=""
+              onClick={(e) => e.stopPropagation()}
               className="appearance-none bg-gray-50 rounded-full pl-4 pr-8 py-1.5 text-sm font-medium text-amber-900 shadow-sm focus:outline-none focus:ring-1 focus:ring-saffron/50 transition-all duration-300 cursor-pointer"
             >
               <option value="" className="bg-amber-50 text-amber-900 text-sm">
@@ -72,6 +96,7 @@ const HeroSection = () => {
         <div className="flex justify-end">
           <Link
             href="/statistics"
+            onClick={(e) => e.stopPropagation()}
             className="px-4 py-2 hover:bg-gray-100 bg-white outline-1 outline-amber-800 rounded-md fixed right-10 bottom-10"
           >
             Statistics
